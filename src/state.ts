a2a09b99e59ae860b1ae85f937a33a4157521fee
Makisuo <minetoaster12@gmail.com>
2025-07-16 01:27:09 +0200

    fix

--- src/state.ts
+++ src/state.ts
@@ -1,19 +1,19 @@
-import { User } from "@workos-inc/authkit-js";
+import type { User } from "@workos-inc/authkit-js";
 
 export interface State {
-  isLoading: boolean;
-  user: User | null;
-  role: string | null;
-  organizationId: string | null;
-  permissions: string[];
-  featureFlags: string[];
+	isLoading: boolean;
+	user: User | null;
+	role: string | null;
+	organizationId: string | null;
+	permissions: string[];
+	featureFlags: string[];
 }
 
 export const initialState: State = {
-  isLoading: true,
-  user: null,
-  role: null,
-  organizationId: null,
-  permissions: [],
-  featureFlags: [],
+	isLoading: true,
+	user: null,
+	role: null,
+	organizationId: null,
+	permissions: [],
+	featureFlags: [],
 };
